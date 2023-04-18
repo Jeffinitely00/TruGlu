7a0d255cf90d8da88154472c3d7739d3146955a3
Jeff <jeff_lim408@icloud.com>
2023-04-18 15:36:33 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from 'react'
+import { useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+    useEffect(() => {
+      Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=10').then((response) => {
+        console.log(response.data);
+      })
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <div className="cryptoHeader"></div>
+    <div className="cryptoDisplay"></div>
+  </div>
+  )
 }
 
 export default App;
