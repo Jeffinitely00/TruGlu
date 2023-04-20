2cf8928537b8b339d01ea58a810f8e9ab9d95672
Jeff <jeff_lim408@icloud.com>
2023-04-20 13:25:50 -0500

    last commit

--- src/App.js
+++ src/App.js
@@ -6,28 +6,42 @@ import Coin from "./components/Coin";
 
 function App() {
   const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
   useEffect(() => {
-    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
       (response) => {
         setListOfCoins(response.data.coins);
       }
     );
   }, []);
+
+  // loop through listOfCoins, Keeps the coin.name in the list that matches the searchWord input
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <div className="cryptoHeader"></div>
-      {/* Loop/map through each object in listOfCoins, and return Coin props */}
+      <div className="cryptoHeader">
+        {/**set the value of the state to be equal to input*/}
+        <input
+          type="text"
+          placeholder="Search coin..."
+          onChange={(event) => {
+            setSearchWord(event.target.value);
+          }}
+        />
+      </div>
+      {/* Loop/map through each object in filteredCoins, and return Coin props */}
       <div className="cryptoDisplay">
-        {listOfCoins.map((coin) => {
+        {filteredCoins.map((coin) => {
           return (
             <Coin
               name={coin.name}
               icon={coin.icon}
               price={coin.price}
               symbol={coin.symbol}
-              priceChange1h={coin.priceChange1h}
-              priceChange1d={coin.priceChange1d}
-              priceChange1w={coin.priceChange1w}
             />
           );
         })}
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
-function Coin({name, icon, price, symbol, priceChange1h, priceChange1d, priceChange1w}) {
+function Coin({name, icon, price, symbol,}) {
   return (
     <div className="coin">
         <h1> {name} </h1>
         <img src={icon} />
-        <h3> Price: {price} </h3>
+        <h3> Price(USD): {price} </h3>
         <h3> Symbol: {symbol} </h3>
-        {/* <div>
-            <h5>1h Change: {priceChange1h}</h5>
-            <h5>24h Change: {priceChange1d}</h5>
-            <h5>7d Change: {priceChange1w}</h5>
-        </div> */}
     </div>
   )
 }
